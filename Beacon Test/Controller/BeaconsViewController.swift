f743625ab48da59d886dd81110c501607e061e7f
Juan Colilla <jcolilla@everis.com>
2020-01-29 17:42:47 +0100

    Documentación actualizada.

--- Beacon Test/Controller/BeaconsViewController.swift
+++ Beacon Test/Controller/BeaconsViewController.swift
@@ -31,9 +31,6 @@ class BeaconsViewController: UIViewController, CLLocationManagerDelegate {
     /// Array de vistas destinadas a contener una imagen que represente de forma visual la intensidad de señal de los beacons detectados.
     @IBOutlet var imageViews: [UIImageView]!
     @IBOutlet var valuesDisplay: [UITextField]!
-    
-    
-    //@IBOutlet weak var beaconImageViews: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,15 +40,12 @@ class BeaconsViewController: UIViewController, CLLocationManagerDelegate {
         beaconManager.setRegion()
         beaconManager.scanForBeacons()
         
-        
-        //imageViews.forEach { $0.image = UIImage(named: "Beacon_OFF") } //-> Establece todas las imágenes en BEACON_OFF cuando la primera vista se carga.
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         beaconManager.checkUserPermissions()
     }
 
-    // Escanea continuamente beacons próximos y proporciona un array con estos.
     func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
         
         // Filtramos los beacons cuya intensidad de señal sea tan débil que no nos sirvan de nada.
@@ -74,17 +68,17 @@ class BeaconsViewController: UIViewController, CLLocationManagerDelegate {
     
     // Se necesita una función que compruebe la ubicación del usuario y en base a cambios en esta hable y diga si se ha entrado o salido de la zona que se quiere monitorizar (punto de interés). Podría ser posible hacer esto sin necesidad de la ubicación? Basándose en la disponibilidad de un beacon?
     
-    // Detecta la entrada en una región monitorizada. -> Estos dos métodos no funcionan en base a un perímetro viertual bluetooth, sino en base a un perímetro geodelimitado (GPS).
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         sendNotification(mode: "entry", region: region)
     }
-    
-    // Detecta la salida de una región monitorizada.
+
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
         sendNotification(mode: "exit", region: region)
     }
     
-    // Clasifica los beacons detectados y los distribuye en la pantalla asignandoles una imagen en base a su intensidad de señal.
+    /**
+     Clasifica los beacons detectados y los distribuye en la pantalla asignandoles una imagen en base a su intensidad de señal.
+     */
     func orderAndDisplayBeaconsOnScreen(beacon: CLBeacon, imageView: UIImageView, display: UITextField) {
         switch beacon.accuracy {
         case 0...1.5:
